8a95996390e81d0fe7cc043e5e1df6e5d4d1e779
sliceoflif3 <yunomiclover@gmail.com>
2023-08-09 07:55:38 +0700

    add accelerator

---
+++ Racer/Assets/Scripts/CarController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float maxSpeed;
+
+    public float forwardAccel = 8f, reverseAccel = 4f;
+
+    private float speedInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel;
+        } else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel;
+        }
+
+        transform.position = theRB.position;
+    }
+
+    private void FixedUpdate()
+    {
+        theRB.AddForce(new Vector3(0f, 0f, speedInput * 1000));
+    }
+}
